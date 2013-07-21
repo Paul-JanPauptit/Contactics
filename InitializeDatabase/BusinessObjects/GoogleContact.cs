f700692c2694a4585397d9f5553194f75731ad58
Paul-Jan Pauptit <paul-jan@depauptits.nl>
2013-07-21 15:26:38 +0200

    Various

---
+++ InitializeDatabase/BusinessObjects/GoogleContact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace InitializeDatabase.BusinessObjects
+{
+    public class GoogleContact
+    {
+        public String Name { get; set; }
+
+        public String Address1Formatted { get; set; }
+
+        public String Phone1Value { get; set; }
+
+        public String Email1Value { get; set; }
+    }
+
+    public sealed class GoogleContactMap : CsvClassMap<GoogleContact>
+    {
+        public override void CreateMap()
+        {
+            Map(m => m.Name).Name("Name");
+            Map(m => m.Address1Formatted).Name("Address 1 - Formatted");
+            Map(m => m.Phone1Value).Name("Phone 1 - Value");
+            Map(m => m.Email1Value).Name("E-mail 1 - Value");
+        }
+    }
+
+}
---
+++ InitializeDatabase/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using InitializeDatabase.BusinessObjects;
+using System.IO;
+using CsvHelper;
+using Contactisch;
+
+class Program
+{
+    public static void Main()
+    {
+        using ( TextReader textReader = File.OpenText( @"..\..\..\Data\contacts.csv" ))
+        using (var context = new ContacticsContext())
+        {
+            var csvReader = new CsvReader( textReader );
+            csvReader.Configuration.RegisterClassMap<GoogleContactMap>();
+            var contacts = csvReader.GetRecords<GoogleContact>();
+
+            ImportGoogleContacts(contacts, context);
+        }
+    }
+
+    private static void ImportGoogleContacts(IEnumerable<GoogleContact> aContacts, ContacticsContext aContext)
+    {
+        
+        // INFO, PJ: Clear table. Entity framework doesn't do bulk deletes. 
+        aContext.Database.ExecuteSqlCommand("delete from Contacts");
+
+        // INFO, PJ: Import all contacts
+        foreach (var googleContact in aContacts)
+        {
+            if (!String.IsNullOrWhiteSpace(googleContact.Name))
+            {
+                Contact contact = new Contact()
+                {
+                    UserId = 1,
+                    Address = googleContact.Address1Formatted,
+                    Email = googleContact.Email1Value,
+                    FullName = googleContact.Name,
+                    Phone = googleContact.Phone1Value
+                };
+
+                aContext.Contacts.Add(contact);
+                aContext.SaveChanges();
+            }
+        }
+    }
+
+
+}
+
